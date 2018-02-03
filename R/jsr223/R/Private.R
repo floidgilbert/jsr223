94126bc81ca708a01a4ab3c1a2825d057f3d427c
Floid Gilbert <floidgilbert@gmail.com>
2018-02-02 21:27:27 -0700

    nc

--- R/jsr223/R/Private.R
+++ R/jsr223/R/Private.R
@@ -23,8 +23,7 @@ strintrplt <- function (snippet, envir = parent.frame()) {
   m <- regexpr("@\\{([^\\}]+)\\}", snippet)
   if (m != -1) {
     s1 <- substr(snippet, 1, m - 1)
-    s2 <- substr(snippet, m + 2, m + attr(m, "match.length") -
-        2)
+    s2 <- substr(snippet, m + 2, m + attr(m, "match.length") - 2)
     s3 <- substr(snippet, m + attr(m, "match.length"), nchar(snippet))
     strintrplt(paste(s1, paste(toString(eval(parse(text = s2),
       envir = envir)), collapse = " ", sep = ""), s3, sep = ""),
