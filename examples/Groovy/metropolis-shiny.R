0f262adb61c9155e07b2a04356193656b4a14216
Floid Gilbert <floidgilbert@gmail.com>
2018-03-23 22:06:22 -0600

    nc

--- examples/Groovy/metropolis-shiny.R
+++ examples/Groovy/metropolis-shiny.R
@@ -1,6 +1,5 @@
 # Initialize --------------------------------------------------------------
 
-
 library("shiny")
 library("jsr223")
 
@@ -22,6 +21,23 @@ starting.values <- rbind(
   c(0.999, 30)
 )
 
+# Set global variables in the Groovy environment.
+#
+# `alpha`, `beta`, `theta`, and `kappa` are parameters for the Beta and Gamma
+# priors, respectively. The are used to define the posterior function.
+#
+# `data` is an array of the data values. In this case, counts.
+#
+# `proposalVariances` are the variance parameters for the proposal distributions
+# (both Gaussian).
+#
+# `startingValues` are the starting values for each random walk.
+#
+# `iterations` are the number of iterations for each random walk.
+#
+# `threads` the number of threads to use. Chains are allocated to threads. If
+# there are more chains than threads, the threads will be recycled.
+
 engine$alpha <- 1
 engine$beta <- 1
 engine$theta <- 2
@@ -29,11 +45,19 @@ engine$kappa <- 1
 engine$data <- as.integer(c(rep(0, 25), rep(1, 6), rep(2, 4), rep(3, 3), 5))
 engine$proposalVariances <- c(0.5^2, 1.7^2)
 engine$startingValues <- starting.values
-engine$iterations <- 10000L
+engine$iterations <- 20000L
+engine$discard <- as.integer(engine$iterations * 0.10)
 engine$threads <- parallel::detectCores()
-engine$setArrayOrder("column-minor")
-cs <- engine$compileSource("metropolis-hastings.groovy")
 
+# Set the array order to get the results in the form we like. See the
+# documentation for more information on array order settings.
+engine$setArrayOrder("column-minor")
+
+# Compile the Groovy script to Java byte code. This approach is recommended only
+# for unstructured code (i.e., code not encapsulated in methods or functions).
+# Otherwise, define functions/methods and call them with engine$invokeMethod or
+# engine$invokeFunction.
+cs <- engine$compileSource("metropolis.groovy")
 
 # Shiny -------------------------------------------------------------------
 
@@ -102,13 +126,15 @@ server <- function(input, output) {
       par(mfrow = c(4, 1))
       curve(dbeta(x,  input$betaShape1, input$betaShape2), 0, 1, ylab = "", xlab = "", main = "Beta", col = "darkorange3", lwd = 2)
       curve(dgamma(x, input$gammaShape, rate = input$gammaRate), 0, 15, ylab = "", xlab = "", main = "Gamma", col = "firebrick", lwd = 2)
-      hist(pi, breaks = 50, freq = FALSE, main = "pi", xlab = "", xlim = c(0, 1), col = "orange")
-      hist(lambda, breaks = 50, freq = FALSE, main = "lambda", xlab = "", xlim = c(0, 4), col = "darkolivegreen1")
-      par(mfrow = c(1, 1))
       q <- c(0.025, 0.5, 0.975)
       qtable <- rbind(pi = quantile(pi, q), lambda = quantile(lambda, q))
       qtable <- cbind(qtable, Width = qtable[, 3] - qtable[, 1])
       output$table <- renderTable(qtable, rownames = TRUE, digits = 2)
+      hist(pi, breaks = 50, freq = FALSE, main = "pi", xlab = "", xlim = c(0, 1), col = "orange")
+      abline(v = qtable[1, 1:3], lwd = 2)
+      hist(lambda, breaks = 50, freq = FALSE, main = "lambda", xlab = "", xlim = c(0, 4), col = "darkolivegreen1")
+      abline(v = qtable[2, 1:3], lwd = 2)
+      par(mfrow = c(1, 1))
     }
   )
 }
